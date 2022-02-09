859fa97882c2c3e10a6617847397ed2a2470b1dd
Lemine <lemine.el.agheb1994@gmail.com>
2022-02-09 05:49:59 +0100

    implemented total duration

--- src/main/tech.goodquestion.parser/LocalDateTimeParser.kt
+++ src/main/tech.goodquestion.parser/LocalDateTimeParser.kt
@@ -1,11 +1,11 @@
 import java.time.LocalDateTime
+import java.time.Period
 import java.time.format.DateTimeFormatter
 import java.time.temporal.ChronoUnit
 import java.util.*
-
+import kotlin.math.abs
 
 class LocalDateTimeParser private constructor() {
-
 
     companion object Parser {
 
@@ -19,7 +19,7 @@ class LocalDateTimeParser private constructor() {
             "today" to LocalDateTime.now(),
             "yesterday" to LocalDateTime.now().minusDays(1),
             "valentine" to LocalDateTime.of(LocalDateTime.now().year, 2, 14, 0, 0, 0),
-
+            "corona" to LocalDateTime.of(2019, 12, 1, 0, 0, 0),
 
             )
 
@@ -36,35 +36,32 @@ class LocalDateTimeParser private constructor() {
                 }
             }
 
-
-            var years = 0
-            var months = 0
-            var weeks = 0
-            var days = 0
-            var hours = 0
-            var minutes = 0
-            var seconds = 0
+            var years:Long = 0
+            var months:Long = 0
+            var weeks:Long = 0
+            var days:Long = 0
+            var hours:Long= 0
+            var minutes:Long = 0
+            var seconds:Long = 0
 
 
             val args: MutableList<String> = cleanedHumanInput.trim().split(" ") as MutableList<String>
 
-            if ((args.size > 1).not()) throw UnsupportedOperationException("Invalid Input")
-
+            if (args.size == 1) throw IllegalArgumentException("Invalid Input")
 
             args.removeAll(listOf(""))
-
 
             for (arg: String in args) {
 
                 try {
 
-                    if (arg.startsWith("y")) years = args[args.indexOf(arg) - 1].toInt()
-                    if (arg.startsWith("mo")) months = args[args.indexOf(arg) - 1].toInt()
-                    if (arg.startsWith("w")) weeks = args[args.indexOf(arg) - 1].toInt()
-                    if (arg.startsWith("d")) days = args[args.indexOf(arg) - 1].toInt()
-                    if (arg.startsWith("h")) hours = args[args.indexOf(arg) - 1].toInt()
-                    if (arg.startsWith("mi")) minutes = args[args.indexOf(arg) - 1].toInt()
-                    if (arg.startsWith("s")) seconds = args[args.indexOf(arg) - 1].toInt()
+                    if (arg.startsWith("y"))  years = args[args.indexOf(arg) - 1].toLong()
+                    if (arg.startsWith("mo")) months = args[args.indexOf(arg) - 1].toLong()
+                    if (arg.startsWith("w")) weeks = args[args.indexOf(arg) - 1].toLong()
+                    if (arg.startsWith("d")) days = args[args.indexOf(arg) - 1].toLong()
+                    if (arg.startsWith("h")) hours = args[args.indexOf(arg) - 1].toLong()
+                    if (arg.startsWith("mi")) minutes = args[args.indexOf(arg) - 1].toLong()
+                    if (arg.startsWith("s")) seconds = args[args.indexOf(arg) - 1].toLong()
 
                 } catch (numberFormatException: NumberFormatException) {
 
@@ -73,24 +70,22 @@ class LocalDateTimeParser private constructor() {
                 }
             }
 
-
             return LocalDateTime.now()
-                .plusYears(years.toLong())
-                .plusMonths(months.toLong())
-                .plusWeeks(weeks.toLong())
-                .plusDays(days.toLong())
-                .plusHours(hours.toLong())
-                .plusMinutes(minutes.toLong())
-                .plusSeconds(seconds.toLong())
+                .plusYears(years)
+                .plusMonths(months)
+                .plusWeeks(weeks)
+                .plusDays(days)
+                .plusHours(hours)
+                .plusMinutes(minutes)
+                .plusSeconds(seconds)
         }
 
-        fun toGermanDateTimeFormat(localDateTime: LocalDateTime): String {
+        fun toGermanFormat(localDateTime: LocalDateTime): String {
 
             val dateFormatter = DateTimeFormatter.ofPattern("dd-MM-yyyy HH:mm").withLocale(Locale.GERMAN)
 
             return localDateTime.format(dateFormatter)
         }
-
 
         private fun removeSpecialCharacters(string: String): String {
 
@@ -98,39 +93,49 @@ class LocalDateTimeParser private constructor() {
         }
 
 
-        fun getDurationUntilPassedDateTimeInSeconds(localDateTime: LocalDateTime): Long {
+        fun getDurationUntilInSeconds(localDateTime: LocalDateTime): Long {
 
             return ChronoUnit.SECONDS.between(LocalDateTime.now(), localDateTime)
         }
 
-        fun getDurationUntilPassedDateTimeInMinutes(localDateTime: LocalDateTime): Long {
+        fun getDurationUntilInMinutes(localDateTime: LocalDateTime): Long {
 
             return ChronoUnit.MINUTES.between(LocalDateTime.now(), localDateTime)
         }
 
-        fun getDurationUntilPassedDateTimeInHours(localDateTime: LocalDateTime): Long {
+        fun getDurationUntilInHours(localDateTime: LocalDateTime): Long {
 
             return ChronoUnit.HOURS.between(LocalDateTime.now(), localDateTime)
         }
 
-        fun getDurationUntilPassedDateTimeInDays(localDateTime: LocalDateTime): Long {
+        fun getDurationUntilInDays(localDateTime: LocalDateTime): Long {
 
             return ChronoUnit.DAYS.between(LocalDateTime.now(), localDateTime)
         }
 
-        fun getDurationUntilPassedDateTimeInMonths(localDateTime: LocalDateTime): Long {
+        fun getDurationUntilInWeeks(localDateTime: LocalDateTime): Long {
+
+            return ChronoUnit.WEEKS.between(LocalDateTime.now(), localDateTime)
+        }
+
+        fun getDurationUntilInMonths(localDateTime: LocalDateTime): Long {
 
             return ChronoUnit.MONTHS.between(LocalDateTime.now(), localDateTime)
         }
-
-        fun getDurationUntilPassedDateTimeInYears(localDateTime: LocalDateTime): Long {
+        
+        fun getDurationUntilInYears(localDateTime: LocalDateTime): Long {
 
             return ChronoUnit.YEARS.between(LocalDateTime.now(), localDateTime)
         }
 
-        fun getDurationUntilPassedDateTime(localDateTime: LocalDateTime): String {
+        fun getDurationUntil(localDateTime: LocalDateTime): String {
 
-            TODO()
+            val period = Period.between(LocalDateTime.now().toLocalDate(), localDateTime.toLocalDate())
+            val years = abs(period.years)
+            val months = abs(period.months)
+            val days = abs(period.days)
+
+            return "$years years $months months $days days"
         }
 
         fun getSpecialDays(): Set<String> {
--- src/main/tech.goodquestion.parser/Main.kt
+++ src/main/tech.goodquestion.parser/Main.kt
@@ -1,11 +1,12 @@
 import java.time.LocalDateTime
 
-
 fun main(){
 
+    val localDateTime: LocalDateTime? = LocalDateTimeParser.parse( "2 months 6   years 4 d 4 hours 2 minutes")
 
+    println(localDateTime?.let { LocalDateTimeParser.toGermanFormat(it) })
 
-    println(LocalDateTimeParser.parse("tomorrow"))
+    println(localDateTime?.let { LocalDateTimeParser.getDurationUntil(it) })
 
 
 
